6cb9607db2ed0f0e15be7d3bf75b3afc665708e7
Maksim Łabovič <maksim.labovicz@gmail.com>
2016-02-20 16:09:19 +0300

    login and restore + styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,114 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+	function addClass(el, cls) {
+		if (el.classList) {
+			el.classList.add(cls);
+		}	else {
+			el.className.baseVal += ' ' + cls;
+		}
+	}
+
+
+	function removeClass(el, cls) {
+		if (el.classList) {
+			el.classList.remove(cls);
+		} else {
+			el.className.baseVal = el.className.baseVal.replace(new RegExp('(^|\\b)' + cls.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		}
+	}
+
+	function hasClass(el, cls) {
+		let result = false;
+		if (el.classList) {
+			result = el.classList.contains(cls);
+			console.log(el.classList.contains(cls));
+		}	else {
+			result = new RegExp('(^| )' + cls + '( |$)', 'gi').test(el.className);
+		}
+		return result;
+	}
+
+	function toggleClass(el, cls) {
+		if (el.classList) {
+			el.classList.toggle(cls);
+		} else {
+			const classes = el.className.split(' ');
+			const existingIndex = classes.indexOf(cls);
+
+			if (existingIndex >= 0) {
+				classes.splice(existingIndex, 1);
+			} else {
+				classes.push(cls);
+			}
+
+			el.className = classes.join(' ');
+		}
+	}
+
+	// click everywhere — close dropdowns
+	$(document).click( function ( ev ) {
+		if ( $(ev.target).closest('.js-main-menu-item').length === 0 ) {
+			$('#js-top-menu').find('.js-main-menu-item.is-selected').removeClass('is-selected');
+		}
+	});
+
+
+	// check section
+	$('.js-check-section').on('click', function ( ev ) {
+		const selected = 'check-section--is-selected';
+		const self = ev.target;
+		toggleClass(self, selected);
+	});
+
+
+	// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+	if (!String.prototype.trim) {
+		(function () {
+			// Make sure we trim BOM and NBSP
+			const rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+			String.prototype.trim = function () {
+				return this.replace(rtrim, '');
+			};
+		})();
+	}
+
+
+	function onInputBlur( ev ) {
+		if (ev.target.value.trim() === '') {
+			removeClass( ev.target, 'field--is-filled' );
+		} else {
+			addClass(ev.target, 'field--is-filled');
+		}
+	}
+
+	[].slice.call( document.querySelectorAll( 'input.field' ) ).forEach( function ( inputEl ) {
+		// in case the input is already filled..
+		if (inputEl.value.trim() !== '') {
+			addClass( inputEl, 'field--is-filled' );
+		}
+
+		// events:
+		inputEl.addEventListener( 'blur', onInputBlur );
+	} );
+
+	const openedMenu = 'btn__menu--is-opened';
+	$('.js-btn-toggle-menu').hover(function () {
+		console.log('mouseenter');
+		const parent = $(this).closest('.js-btn-with-menu');
+		const menu = parent.find('.js-btn-menu');
+		menu.addClass(openedMenu);
+	}, function () {
+		const parent = $(this).closest('.js-btn-with-menu');
+		const menu = parent.find('.js-btn-menu');
+		console.log('mouseleave');
+		menu.removeClass(openedMenu);
+	});
+
+	$('.js-btn-with-menu').on('click', '.js-btn-menu a', function () {
+		const menu = $(this).closest('.js-btn-menu');
+		menu.removeClass(openedMenu);
+	});
+
 });
--- gulp/styles.js
+++ gulp/styles.js
@@ -5,26 +5,52 @@ import gulpif from 'gulp-if';
 import rupture from 'rupture';
 import stylint from 'gulp-stylint';
 import stylus from 'gulp-stylus';
-import autoprefixer from 'autoprefixer-stylus';
+// import autoprefixer from 'autoprefixer-stylus';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import cssgrace from 'cssgrace';
+import rgbaFallback from 'postcss-color-rgba-fallback';
+import fontWeight from 'postcss-font-weights';
 import gcmq from 'gulp-group-css-media-queries';
 import nano from 'gulp-cssnano';
 import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
 import errorHandler from 'gulp-plumber-error-handler';
+import { browsers } from '../package.json';
 
 gulp.task('styles', () => (
 	gulp.src('app/styles/*.styl')
 		.pipe(plumber({errorHandler: errorHandler('Error in \'styles\' task')}))
 		.pipe(gulpif(gutil.env.debug, sourcemaps.init()))
+		// .pipe(stylus({
+		// 	use: [
+		// 		rupture(),
+		// 		autoprefixer()
+		// 	],
+		// 	'include css': true
+		// }))
 		.pipe(stylus({
 			use: [
-				rupture(),
-				autoprefixer()
+				rupture()
 			],
 			'include css': true
 		}))
+		.pipe(postcss([
+			rgbaFallback(),
+			autoprefixer(
+					'Android >= ' + browsers.android,
+					'Chrome >= ' + browsers.chrome,
+					'Firefox >= ' + browsers.firefox,
+					'Explorer >= ' + browsers.ie,
+					'iOS >= ' + browsers.ios,
+					'Opera >= ' + browsers.opera,
+					'Safari >= ' + browsers.safari
+				),
+			fontWeight()
+			]
+		))
 		.pipe(gulpif(!gutil.env.debug, gcmq()))
-		.pipe(gulpif(!gutil.env.debug, nano()))
+		// .pipe(gulpif(!gutil.env.debug, nano()))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(gulpif(gutil.env.debug, sourcemaps.write()))
 		.pipe(gulp.dest('dist/assets/styles'))
